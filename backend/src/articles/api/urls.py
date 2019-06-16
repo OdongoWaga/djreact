9d0733449dab5b79ff5f4c1d1a66958bb567ec9a
Waga <wagaodongo@gmail.com>
2019-06-16 20:14:50 +0300

    authentication

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,9 +1,46 @@
-from django.urls import path
+# from rest_framework import viewsets
 
-from .views import ArticleListView, ArticleDetailView
+# class ArticleViewSet(viewsets.ModelViewSet):
+#     serializer_class = ArticleSerializer
+#     queryset = Article.objects.all()
 
-urlpatterns =[
-    path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
+from rest_framework import permissions
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView
+)
+from articles.models import Article
+from .serializers import ArticleSerializer
 
-]
+
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.AllowAny, )
+
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.AllowAny, )
+
+
+class ArticleCreateView(CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class ArticleUpdateView(UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class ArticleDeleteView(DestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated, )
